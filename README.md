488cea8e762d967b302aea2d3e13e55762127cc9
Trusted Subject <trustedsubject@gmail.com>
2016-09-19 10:18:01 +1000

    Initial release of POC source with distinctfields command.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # setops
 Set Operations App for Splunk
 
-This app seeks to address: https://answers.splunk.com/answers/258504/how-to-count-the-the-number-of-elements-in-a-field.html
+This is the intial POC version shown here: https://www.youtube.com/watch?v=Z6w-VG2CpP0 and seeks to address: https://answers.splunk.com/answers/258504/how-to-count-the-the-number-of-elements-in-a-field.html
 
-Please note, sourcecode is to be released shortly.
+N.B. The bin/splunklib folder is provided by the Splunk SDK: https://github.com/splunk/splunk-sdk-python/tree/master/splunklib
---
+++ bin/distinctfields.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import sys
+from splunklib.searchcommands import \
+    dispatch, EventingCommand, Configuration, Option, validators
+
+from collections import defaultdict
+
+@Configuration()
+class DistinctCommand(EventingCommand):
+    """ %(synopsis)
+
+    ##Syntax
+
+    %(syntax)
+
+    ##Description
+
+    %(description)
+
+    """
+    def transform(self, events):
+        eventnum = 0
+        x = {}
+        fieldnames = self.fieldnames
+
+        workingset = []
+        for event in events:
+            workingset.append(event)
+
+        for field in fieldnames:
+            x[field] = defaultdict(set)
+
+        for event in workingset:
+            for field in fieldnames:
+                for element in event[field]:
+                    x[field][element].add(eventnum)
+            event['distinctfields'] = []
+	    eventnum += 1
+
+        for field in fieldnames:
+            for element in x[field]:
+                if len(x[field][element]) == 1:
+                    workingset[x[field][element].pop()]['distinctfields'] += [field]
+
+        for event in workingset:
+            yield event
+
+dispatch(DistinctCommand, sys.argv, sys.stdin, sys.stdout, __name__)
