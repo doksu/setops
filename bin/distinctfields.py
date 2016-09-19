81571381d70050add3b4ea9b049847d7864d6c0e
Trusted Subject <trustedsubject@gmail.com>
2016-09-19 11:35:50 +0000

    Changed distinctfields.py to a map/reduce command.

--- bin/distinctfields.py
+++ bin/distinctfields.py
@@ -2,12 +2,12 @@
 
 import sys
 from splunklib.searchcommands import \
-    dispatch, EventingCommand, Configuration, Option, validators
+    dispatch, ReportingCommand, Configuration, Option, validators
 
 from collections import defaultdict
 
-@Configuration()
-class DistinctFieldsCommand(EventingCommand):
+@Configuration(requires_preop=True)
+class DistinctFieldsCommand(ReportingCommand):
     """ %(synopsis)
 
     ##Syntax
@@ -19,8 +19,9 @@ class DistinctFieldsCommand(EventingCommand):
     %(description)
 
     """
-    def transform(self, events):
+    def map(self, events):
         eventnum = 0
+        eventcount = 0
         x = {}
         fieldnames = self.fieldnames
 
@@ -29,31 +30,39 @@ class DistinctFieldsCommand(EventingCommand):
         if len(fieldnames) < 2:
             raise Exception('Please specify at least two fields')
 
-        workingevents = []
-        for event in events:
-            workingevents.append(event)
-
         for field in fieldnames:
             x[field] = defaultdict(set)
 
-        for event in workingevents:
+        for event in events:
+            eventcount += 1
             for field in fieldnames:
                 try:
                     for element in event[field]:
                         x[field][element].add(eventnum)
                 except KeyError:
                     pass
-            event['distinctfields'] = []
 	    eventnum += 1
 
-        for field in fieldnames:
-            for element in x[field]:
-                if len(x[field][element]) == 1:
-                    eventnumber = x[field][element].pop()
-                    if field not in workingevents[eventnumber]['distinctfields']:
-                        workingevents[eventnumber]['distinctfields'] += [field]
+        distinctfields = [[] for i in range(eventcount)]
 
-        for event in workingevents:
+        for field in x:
+            for element in [field]:
+                if len(x[field][element] == 1:
+                    eventnumber = x[field][element].pop()
+                    if field not in distinctfields[eventnumber]:
+                        distinctfields[eventnumber] += [field]
+
+        yield {self.distinctfields: distinctfields}
+
+    def reduce(self, records):
+        eventnum = 0
+        for event in events:
+            event['distinctfields'] = self.distinctfields[eventnum]
+            eventnum += 1
             yield event
+
+    def __init__(self):
+        super(DistinctFieldsCommand, self).__init__()
+        self.distinctfields = None
 
 dispatch(DistinctFieldsCommand, sys.argv, sys.stdin, sys.stdout, __name__)
