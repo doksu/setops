5291e3ac7ccf341c0cb7d7d82ede6cec11cf3f5d
Trusted Subject <trustedsubject@gmail.com>
2017-02-22 10:26:03 +1000

    Added working (but in much need of refinement) first crack at setop command.

---
+++ bin/setop.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
+import sys
+
+
+@Configuration()
+class SetOpCommand(StreamingCommand):
+    """ 
+
+    ##Syntax
+
+
+    ##Description
+
+
+    ##Example
+
+
+    """
+    op = Option(
+        doc='''
+        **Syntax:** **op=***<cardinality|relation|union|intersection|diff|sym_diff|>*
+        **Description:** ''',
+        require=True)
+
+    def stream(self, records):
+        if self.op == 'cardinality':
+            for record in records:
+                record['cardinality'] = len(set(record[self.fieldnames[0]]))
+                yield record
+        elif self.op == 'relation':
+            for record in records:
+                if set(record[self.fieldnames[0]]) == set(record[self.fieldnames[1]]):
+                    relation = "equal"
+                elif set(record[self.fieldnames[0]]).issubset(set(record[self.fieldnames[1]])):
+                    relation = "subset"
+                elif set(record[self.fieldnames[0]]).issuperset(set(record[self.fieldnames[1]])):
+                    relation = "superset"
+                elif len(set(record[self.fieldnames[0]]).intersection(set(record[self.fieldnames[1]]))) > 0:
+                    relation = "partially disparate"
+                else:
+                    relation = "fully disparate"
+                record["relation"] = relation
+                yield record
+        elif self.op == 'union':
+            for record in records:
+                record['union'] = list(set(record[self.fieldnames[0]]).union(set(record[self.fieldnames[1]])))
+                yield record
+        elif self.op == 'intersection':
+            for record in records:
+                record['intersection'] = list(set(record[self.fieldnames[0]]).intersection(set(record[self.fieldnames[1]])))
+                yield record
+        elif self.op == 'diff':
+            for record in records:
+                record['diff'] = list(set(record[self.fieldnames[0]]).difference(set(record[self.fieldnames[1]])))
+                yield record
+        elif self.op == 'sym_diff':
+            for record in records:
+                record['sym_diff'] = list(set(record[self.fieldnames[0]]).symmetric_difference(set(record[self.fieldnames[1]])))
+                yield record
+        else:
+            raise Exception("Invalid op type")
+
+dispatch(SetOpCommand, sys.argv, sys.stdin, sys.stdout, __name__)
