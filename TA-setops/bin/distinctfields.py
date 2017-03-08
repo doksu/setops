d5597989a7a5c207ea0c752fdb8021d48d144bd1
Trusted Subject <trustedsubject@gmail.com>
2017-03-08 21:17:59 +1000

    Many improvements to several parts of the app.

--- TA-setops/bin/distinctfields.py
+++ TA-setops/bin/distinctfields.py
@@ -35,24 +35,27 @@ class DistinctFieldsCommand(EventingCommand):
 
         event_cache = []
         tuple_counter = {}
+	eventcount = 0
 
         for event in events:
             event_cache.append(event)
             for by_field in event[self.by]:
                 for field in self.fieldnames:
-                    for field_value in event[field]:
-                        if (by_field, field, field_value) in tuple_counter:
-                            tuple_counter[(by_field, field, field_value)] += 1
-                        else:
-                            tuple_counter[(by_field, field, field_value)] = 1
+                    if field in event:
+                        for field_value in event[field]:
+                            if (by_field, field, field_value) in tuple_counter:
+                                tuple_counter[(by_field, field, field_value)] += 1
+                            else:
+                                tuple_counter[(by_field, field, field_value)] = 1
 
         for event in event_cache:
             event['distinct_fields'] = set()
             for by_field in event[self.by]:
                 for field in self.fieldnames:
-                    for field_value in event[field]:
-                        if tuple_counter[(by_field, field, field_value)] == 1:
-                            event['distinct_fields'].add(field)
+                    if field in event:
+                        for field_value in event[field]:
+                            if tuple_counter[(by_field, field, field_value)] == 1:
+                                event['distinct_fields'].add(field)
             event['distinct_fields'] = list(event['distinct_fields'])
             yield event
 
--- TA-setops/bin/setop.py
+++ TA-setops/bin/setop.py
@@ -19,7 +19,7 @@ class SetOpCommand(StreamingCommand):
     """
     op = Option(
         doc='''
-        **Syntax:** **op=***<cardinality|relation|union|intersection|difference|symmetric_difference|>*
+        **Syntax:** **op=***<cardinality|relation|union|intersection|difference|symmetric_difference>*
         **Description:** ''',
         require=True)
 
@@ -28,47 +28,76 @@ class SetOpCommand(StreamingCommand):
             if len(self.fieldnames) != 1:
                 raise Exception("One field name must be provided for cardinality.")
             for event in events:
-                event['cardinality'] = len(set(event[self.fieldnames[0]]))
+                try:
+                    event['cardinality'] = len(set(event[self.fieldnames[0]]))
+                except:
+                    event['cardinality'] = 0
                 yield event
         elif self.op == 'relation':
             if len(self.fieldnames) != 2:
                 raise Exception("Two field names must be provided.")
             for event in events:
-                if set(event[self.fieldnames[0]]) == set(event[self.fieldnames[1]]):
-                    relation = "equal"
-                elif set(event[self.fieldnames[0]]).issubset(set(event[self.fieldnames[1]])):
-                    relation = "subset"
-                elif set(event[self.fieldnames[0]]).issuperset(set(event[self.fieldnames[1]])):
-                    relation = "superset"
-                elif len(set(event[self.fieldnames[0]]).intersection(set(event[self.fieldnames[1]]))) > 0:
-                    relation = "partially disjoint"
-                else:
-                    relation = "fully disjoint"
+                try:
+                    if set(event[self.fieldnames[0]]) == set(event[self.fieldnames[1]]):
+                        relation = "equal"
+                    elif set(event[self.fieldnames[0]]).issubset(set(event[self.fieldnames[1]])):
+                        relation = "subset"
+                    elif set(event[self.fieldnames[0]]).issuperset(set(event[self.fieldnames[1]])):
+                        relation = "superset"
+                    elif len(set(event[self.fieldnames[0]]).intersection(set(event[self.fieldnames[1]]))) > 0:
+                        relation = "partially disjoint"
+                    else:
+                        relation = "fully disjoint"
+                except:
+                    relation = ""
                 event["relation"] = relation
                 yield event
         elif self.op == 'union':
             if len(self.fieldnames) != 2:
                 raise Exception("Two field names must be provided.")
             for event in events:
-                event['union'] = list(set(event[self.fieldnames[0]]).union(set(event[self.fieldnames[1]])))
+                try:
+                    event['union'] = list(set(event[self.fieldnames[0]]).union(set(event[self.fieldnames[1]])))
+                except:
+                    if self.fieldnames[0] in event:
+                        event['union'] = list(set(event[self.fieldnames[0]]))
+                    elif self.fieldnames[1] in event:
+                        event['union'] = list(set(event[self.fieldnames[1]]))
+                    else:
+                        event['union'] = []
                 yield event
         elif self.op == 'intersection':
             if len(self.fieldnames) != 2:
                 raise Exception("Two field names must be provided.")
             for event in events:
-                event['intersection'] = list(set(event[self.fieldnames[0]]).intersection(set(event[self.fieldnames[1]])))
+                try:
+                    event['intersection'] = list(set(event[self.fieldnames[0]]).intersection(set(event[self.fieldnames[1]])))
+                except:
+                    event['intersection'] = []
                 yield event
         elif self.op == 'difference':
             if len(self.fieldnames) != 2:
                 raise Exception("Two field names must be provided.")
             for event in events:
-                event['difference'] = list(set(event[self.fieldnames[0]]).difference(set(event[self.fieldnames[1]])))
+                try:
+                    event['difference'] = list(set(event[self.fieldnames[0]]).difference(set(event[self.fieldnames[1]])))
+                except:
+                    if self.fieldnames[0] in event:
+                        event['difference'] = list(set(event[self.fieldnames[0]]))
+                    else:
+                        event['difference'] = []
                 yield event
         elif self.op == 'symmetric_difference':
             if len(self.fieldnames) != 2:
                 raise Exception("Two field names must be provided.")
             for event in events:
-                event['symetric_difference'] = list(set(event[self.fieldnames[0]]).symmetric_difference(set(event[self.fieldnames[1]])))
+                try:
+                    event['symmetric_difference'] = list(set(event[self.fieldnames[0]]).symmetric_difference(set(event[self.fieldnames[1]])))
+                except:
+                    if self.fieldnames[0] in event:
+                        event['symmetric_difference'] = list(set(event[self.fieldnames[0]]))
+                    elif self.fieldnames[1] in event:
+                        event['symmetric_difference'] = list(set(event[self.fieldnames[1]]))
                 yield event
         else:
             raise Exception("Invalid op type")
