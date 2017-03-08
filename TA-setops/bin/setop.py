694a04616ec234d8a343d7553bd80dc9f5ffddaf
Trusted Subject <trustedsubject@gmail.com>
2017-03-08 20:24:35 +1000

    Cleaned up code in setop.py without changing algorithm

--- TA-setops/bin/setop.py
+++ TA-setops/bin/setop.py
@@ -3,7 +3,6 @@
 from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
 import sys
 
-
 @Configuration()
 class SetOpCommand(StreamingCommand):
     """ 
@@ -24,43 +23,53 @@ class SetOpCommand(StreamingCommand):
         **Description:** ''',
         require=True)
 
-    def stream(self, records):
-        if len(self.fieldnames) != 2:
-            raise Exception("Two field names must be provided.")
+    def stream(self, events):
         if self.op == 'cardinality':
-            for record in records:
-                record['cardinality'] = len(set(record[self.fieldnames[0]]))
-                yield record
+            if len(self.fieldnames) != 1:
+                raise Exception("One field name must be provided for cardinality.")
+            for event in events:
+                event['cardinality'] = len(set(event[self.fieldnames[0]]))
+                yield event
         elif self.op == 'relation':
-            for record in records:
-                if set(record[self.fieldnames[0]]) == set(record[self.fieldnames[1]]):
+            if len(self.fieldnames) != 2:
+                raise Exception("Two field names must be provided.")
+            for event in events:
+                if set(event[self.fieldnames[0]]) == set(event[self.fieldnames[1]]):
                     relation = "equal"
-                elif set(record[self.fieldnames[0]]).issubset(set(record[self.fieldnames[1]])):
+                elif set(event[self.fieldnames[0]]).issubset(set(event[self.fieldnames[1]])):
                     relation = "subset"
-                elif set(record[self.fieldnames[0]]).issuperset(set(record[self.fieldnames[1]])):
+                elif set(event[self.fieldnames[0]]).issuperset(set(event[self.fieldnames[1]])):
                     relation = "superset"
-                elif len(set(record[self.fieldnames[0]]).intersection(set(record[self.fieldnames[1]]))) > 0:
+                elif len(set(event[self.fieldnames[0]]).intersection(set(event[self.fieldnames[1]]))) > 0:
                     relation = "partially disjoint"
                 else:
                     relation = "fully disjoint"
-                record["relation"] = relation
-                yield record
+                event["relation"] = relation
+                yield event
         elif self.op == 'union':
-            for record in records:
-                record['union'] = list(set(record[self.fieldnames[0]]).union(set(record[self.fieldnames[1]])))
-                yield record
+            if len(self.fieldnames) != 2:
+                raise Exception("Two field names must be provided.")
+            for event in events:
+                event['union'] = list(set(event[self.fieldnames[0]]).union(set(event[self.fieldnames[1]])))
+                yield event
         elif self.op == 'intersection':
-            for record in records:
-                record['intersection'] = list(set(record[self.fieldnames[0]]).intersection(set(record[self.fieldnames[1]])))
-                yield record
+            if len(self.fieldnames) != 2:
+                raise Exception("Two field names must be provided.")
+            for event in events:
+                event['intersection'] = list(set(event[self.fieldnames[0]]).intersection(set(event[self.fieldnames[1]])))
+                yield event
         elif self.op == 'difference':
-            for record in records:
-                record['difference'] = list(set(record[self.fieldnames[0]]).difference(set(record[self.fieldnames[1]])))
-                yield record
+            if len(self.fieldnames) != 2:
+                raise Exception("Two field names must be provided.")
+            for event in events:
+                event['difference'] = list(set(event[self.fieldnames[0]]).difference(set(event[self.fieldnames[1]])))
+                yield event
         elif self.op == 'symmetric_difference':
-            for record in records:
-                record['sym_diff'] = list(set(record[self.fieldnames[0]]).symmetric_difference(set(record[self.fieldnames[1]])))
-                yield record
+            if len(self.fieldnames) != 2:
+                raise Exception("Two field names must be provided.")
+            for event in events:
+                event['symetric_difference'] = list(set(event[self.fieldnames[0]]).symmetric_difference(set(event[self.fieldnames[1]])))
+                yield event
         else:
             raise Exception("Invalid op type")
 
